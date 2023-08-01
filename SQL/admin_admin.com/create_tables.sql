5b746e8796a6b33633eb1550b46e285492cf4c70
Kasai <vitorkasai@estudante.ufscar.br>
2023-08-01 18:23:37 -0300

    Adicionando tipoComponenteCurricular e componenteCurricularMaterial

--- SQL/admin_admin.com/create_tables.sql
+++ SQL/admin_admin.com/create_tables.sql
@@ -130,6 +130,23 @@ CREATE TABLE ComponenteCurricular(
 	PRIMARY KEY (Nome_componente)
 );
 
+-- Tipo Componente Curricular
+
+CREATE TABLE TipoComponenteCurricular(
+    Nome_Completo VARCHAR(100),
+    Tipo_Disciplina VARCHAR(50),
+
+    PRIMARY KEY (Nome_Completo)
+);
+
+-- Componente Curricular Material
+CREATE TABLE componenteCurricularMaterial(
+    Nome_componente VARCHAR(100),
+    Nome_material VARCHAR(100),   
+    data DATE,
+
+    PRIMARY KEY (Nome_componente, Nome_material, data)
+);
 
 /*-------------- FK CONSTRAINTS ------------------ */
 
@@ -188,3 +205,22 @@ FOREIGN KEY (Nome_idioma,Proficiencia) REFERENCES ProficienciaIdiomaComponente(N
 ON DELETE CASCADE
 ON UPDATE CASCADE;
 
+-- Tipo Componente Curricular Constraint
+ALTER TABLE TipoComponenteCurricular 
+ADD CONSTRAINT FK_componenteCurricular
+FOREIGN KEY (Nome_completo) REFERENCES ComponenteCurricular(Nome_componente)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- Componente Curricular Material Constraints
+ALTER TABLE componenteCurricularMaterial 
+ADD CONSTRAINT FK_componenteCurricular
+FOREIGN KEY (Nome_componente) REFERENCES ComponenteCurricular(Nome_componente)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE componenteCurricularMaterial 
+ADD CONSTRAINT FK_material
+FOREIGN KEY (Nome_material, data) REFERENCES Material(Nome_material, data)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
