91713162c3e4c91b9b83d1fa140451dbef597510
Carlos Matos <84243647+Carlos-HRM@users.noreply.github.com>
2023-09-05 20:00:26 -0300

    Correçao dos meus artefatos
    Artefatos Corrigidos

--- SQL/procedures/proc_adicionar_docente_autor.sql
+++ SQL/procedures/proc_adicionar_docente_autor.sql
@@ -1,46 +1,79 @@
 CREATE OR REPLACE PROCEDURE AdicionarDocenteAutor(
     cpf_param VARCHAR(11),
+    primeiro_nome_param VARCHAR(50),
+    sobrenome_param VARCHAR(50),
+    genero_param VARCHAR(20),
+    data_nascimento_param DATE,
+    numero_param INTEGER,
+    complemento_param VARCHAR(100),
+    cep_usuario_param VARCHAR(8),
+    cnpj_ies_associada_param VARCHAR(20),
     data_credenciamento_param DATE,
     curriculo_param VARCHAR(256),
     titulacao_param VARCHAR(16),
     poca_file_param VARCHAR(255),
     vinculo_file_param VARCHAR(255),
     link_cnpq_param VARCHAR(255)
+    
 )
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    INSERT INTO docente_especialista (
-        cpf_docente, 
-        data_credenciamento, 
-        curriculo, 
-        titulacao, 
-        poca_file, 
-        vinculo_file, 
-        link_cnpq, 
-        is_orientador, 
-        is_autor, 
-        is_ministrante, 
-        is_coordenador_pedagogico
+    INSERT INTO usuario (
+        cpf, 
+        primeiro_nome, 
+        sobrenome, 
+        genero, 
+        data_nascimento, 
+        numero, 
+        complemento, 
+        cep_usuario,
+        cnpj_ies_associada
     ) VALUES (
         cpf_param, 
-        data_credenciamento_param, 
-        curriculo_param, 
-        titulacao_param, 
-        poca_file_param, 
-        vinculo_file_param, 
-        link_cnpq_param, 
-        false,  -- is_orientador
-        true,   -- is_autor
-        false,  -- is_ministrante
-        false   -- is_coordenador_pedagogico
+        primeiro_nome_param, 
+        sobrenome_param, 
+        genero_param, 
+        data_nascimento_param, 
+        numero_param, 
+        complemento_param, 
+        cep_usuario_param,
+        cnpj_ies_associada_param
     );
+    
+    INSERT INTO docente_especialista(
+            cpf_docente,
+            data_credenciamento, 
+            curriculo, 
+            titulacao, 
+            poca_file, 
+            vinculo_file, 
+            link_cnpq, 
+            is_orientador, 
+            is_autor, 
+            is_ministrante, 
+            is_coordenador_pedagogico
+        ) VALUES (
+            cpf_param, 
+            data_credenciamento_param, 
+            curriculo_param, 
+            titulacao_param, 
+            poca_file_param, 
+            vinculo_file_param, 
+            link_cnpq_param, 
+            false,  -- is_orientador
+            true,   -- is_autor
+            false,  -- is_ministrante
+            false   -- is_coordenador_pedagogico
+    );
+
 
     COMMIT;
 END;
 $$;
 
-/* CALL AdicionarUsuarioAutor(
+/*
+CALL AdicionarDocenteAutor(
     '12345678901',
     'João',
     'Silva',
@@ -49,7 +82,7 @@ $$;
     123,
     'Apto 301',
     '12345678',
-    '12345678901234',
+    '9876543210',
     '2023-09-04',
     'Currículo do Docente',
     'Doutor',
@@ -57,5 +90,4 @@ $$;
     'vinculo_file.pdf',
     'xxxx'
 );
-
 */
--- SQL/procedures/proc_inserir_endereço.sql
+++ SQL/procedures/proc_inserir_endereço.sql
@@ -6,7 +6,28 @@ CREATE OR REPLACE PROCEDURE InserirEndereco(
     estado_param VARCHAR(2),
     pais_param VARCHAR(50)
 )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO cep_endereco (
+        cep,
+        rua,
+        bairro,
+        cidade,
+        estado,
+        pais
+    ) VALUES (
+        cep_param,
+        rua_param,
+        bairro_param,
+        cidade_param,
+        estado_param,
+        pais_param
+    );
 
+    COMMIT;
+END;
+$$;
 /*CALL InserirEndereco(
     '12345678',
     'Rua da Amostra',
