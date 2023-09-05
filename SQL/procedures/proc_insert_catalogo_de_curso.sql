d9bae159f4a1a4c0d8f553d0b84ca4da4f26a311
Vitor Kasai <vitorkasai@estudante.ufscar.br>
2023-09-05 12:42:43 -0300

    Corrigindo um artefato

--- SQL/procedures/proc_insert_catalogo_de_curso.sql
+++ SQL/procedures/proc_insert_catalogo_de_curso.sql
@@ -1,32 +1,26 @@
--- PROCEDURE)
--- stored procedures inserção catálogo de curso
--- arquivo proc_insert_catalogo_de_curso.sql
-
 CREATE OR REPLACE PROCEDURE insert_catalogo_de_curso(
-    IN p_nome_completo character varying, 
-    IN p_idioma character varying, 
-    IN p_nivel character varying, 
-    IN p_categoria character varying, 
-    IN p_carga_horaria smallint, 
-    IN p_link_ementa character varying
-) LANGUAGE plpgsql AS 
-    $$ DECLARE
-            curso_ja_existe CURSOR FOR 
-                SELECT * FROM curso_idioma WHERE nome_completo = p_nome_completo AND idioma = p_idioma;
-                
-            curso_row curso_idioma%ROWTYPE;
-        BEGIN
-            OPEN curso_ja_existe;
-            
-            FETCH curso_ja_existe INTO curso_row;
-            
-            IF FOUND THEN
-                RAISE EXCEPTION 'Curso com nome % e idioma % já existe!', p_nome_completo, p_idioma;
-            ELSE
-                INSERT INTO curso_idioma (nome_completo, idioma, nivel, categoria, carga_horaria, link_ementa)
-                VALUES (p_nome_completo, p_idioma, p_nivel, p_categoria, p_carga_horaria, p_link_ementa);
-            END IF;
-            
-            CLOSE curso_ja_existe;
-        END;
-    $$;
+    p_nome_completo VARCHAR(256), 
+    p_idioma VARCHAR(16), 
+    p_nivel VARCHAR(16), 
+    p_categoria VARCHAR(16), 
+    p_carga_horaria INT, 
+    p_link_ementa VARCHAR(256)
+) 
+LANGUAGE plpgsql 
+AS $$
+DECLARE
+    curso_exists INT;
+BEGIN
+    -- Verifica se o curso já existe
+    SELECT COUNT(*) INTO curso_exists FROM curso_idioma WHERE nome_completo = p_nome_completo AND idioma = p_idioma;
+
+    -- Se o curso já existe, levanta uma exceção, senão insere o novo curso
+    IF curso_exists > 0 THEN
+        RAISE EXCEPTION 'Curso com nome % e idioma % já existe!', p_nome_completo, p_idioma;
+    ELSE
+        INSERT INTO curso_idioma (nome_completo, idioma, nivel, categoria, carga_horaria, link_ementa)
+        VALUES (p_nome_completo, p_idioma, p_nivel, p_categoria, p_carga_horaria, p_link_ementa);
+    END IF;
+    
+END;
+$$;
