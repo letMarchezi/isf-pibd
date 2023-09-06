5d2aa9cec0882d083db6334d4c9c81f7f018322f
Matheus Silva <matheusagj2009@gmail.com>
2023-09-06 11:18:51 -0300

    Add files via upload

---
+++ ApresentaCatalogosCurso.sql
@@ -0,0 +1,5 @@
+-- Autor: Matheus da Silva Vieira 791085 BCC
+-- ApresentaCatálogosCurso -> View com os dados dos Catálogos de curso
+CREATE OR REPLACE VIEW ApresentaCatalogosCurso AS
+SELECT * FROM curso_idioma;
+
